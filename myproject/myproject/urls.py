8767dd22e2825a6d7c016f534ade9648c369281f
zhangming <357303554@qq.com>
2016-08-02 01:54:30 +0800

    master to master

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import patterns, url, include
 from rest_framework import routers
 from quickstart import views
 
-router = routers.DefaultRouter()
+router = routers.DefaultRouter()  
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
